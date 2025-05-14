c47bc12f0e390508f2aec36fa6fcd4e6f3afc578
thelamer <ryankuba@gmail.com>
2025-05-14 14:28:24 -0400

    update endpoints to compile in cmake 4.0 in pip

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pixelflux",
-    version="1.0.0",
+    version="1.0.1",
     author="Linuxserver.io",
     author_email="ryankuba@gmail.com",
     description="A performant web native pixel delivery pipeline for diverse sources, blending VNC-inspired parallel processing of pixel buffers with flexible modern encoding formats.",
