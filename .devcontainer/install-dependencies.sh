43d19a64aed101adb83b2157f302896ba2197e0d
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-07-02 22:55:08 +0900

    Make final fixes

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -3,15 +3,12 @@
 sudo apt update
 # dependencies
 sudo apt-get install -y \
-  cmake \
   g++ \
-  gcc \
   libjpeg-turbo8-dev \
   libx11-dev \
   libxfixes-dev \
   libxext-dev \
   libx264-dev \
-  make \
   python3-dev \
   python3-pip \
   python3-websockets
--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import setuptools
 from setuptools import setup, Extension
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 screen_capture_module = Extension(
     'pixelflux.screen_capture_module', 
     sources=[
@@ -16,6 +13,9 @@ screen_capture_module = Extension(
     extra_compile_args=['-std=c++17', '-Wno-unused-function']
 )
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pixelflux",
     version="1.2.1",
@@ -24,14 +24,13 @@ setup(
     description="A performant web native pixel delivery pipeline for diverse sources, blending VNC-inspired parallel processing of pixel buffers with flexible modern encoding formats.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    license="MPL-2.0",
+    url="https://github.com/linuxserver/pixelflux",
     packages=setuptools.find_packages(),
     ext_modules=[screen_capture_module],
     classifiers=[
         "Programming Language :: Python :: 3",
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         "Operating System :: POSIX :: Linux",
     ],
     python_requires=">=3.6",
-    zip_safe=False,
 )
